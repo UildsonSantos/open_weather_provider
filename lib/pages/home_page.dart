5b77032c3063774057f5927c36e7af3fbf7a579f
Uildson S. dos Santos <wilder_shon@hotmail.com>
2024-05-09 15:29:42 -0300

    create Form in SearchPage widget

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:open_weather_provider/pages/pages.dart';
 import 'package:open_weather_provider/providers/providers.dart';
 import 'package:provider/provider.dart';
 
@@ -10,11 +11,31 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String? _city;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Weather'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () async {
+              final weatherContext = context.read<WeatherProvider>();
+
+              _city = await Navigator.push(context, MaterialPageRoute(
+                builder: (context) {
+                  return const SearchPage();
+                },
+              ));
+
+              if (_city != null) {
+                weatherContext.fetchWeather(_city!);
+              }
+            },
+          )
+        ],
       ),
       body: const Center(
         child: Text('Home page'),
