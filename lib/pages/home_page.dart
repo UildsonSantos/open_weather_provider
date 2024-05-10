153eb8bc52bd2b0c44102693bfbee5d98fdbc594
Uildson S. dos Santos <wilder_shon@hotmail.com>
2024-05-10 13:49:30 -0300

    add weather loaded state in HomePage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:open_weather_provider/constants/constants.dart';
 import 'package:open_weather_provider/pages/pages.dart';
 import 'package:open_weather_provider/providers/providers.dart';
 import 'package:open_weather_provider/widgets/widgets.dart';
 import 'package:provider/provider.dart';
+import 'package:recase/recase.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -21,6 +23,10 @@ class _HomePageState extends State<HomePage> {
     if (ws.status == WeatherStatus.error) {
       errorDialog(context, ws.error.errMsg);
     }
+  }
+
+  String showTemperature(double temperature) {
+    return '${temperature.toStringAsFixed(2)}°C';
   }
 
   @override
@@ -89,11 +95,95 @@ class _HomePageState extends State<HomePage> {
         ),
       );
     }
-    return Center(
-      child: Text(
-        state.weather.name,
-        style: const TextStyle(fontSize: 18.0),
-      ),
+    return ListView(
+      children: [
+        SizedBox(
+          height: MediaQuery.of(context).size.height / 6,
+        ),
+        Text(
+          state.weather.name,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            fontSize: 40.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 10.0),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              TimeOfDay.fromDateTime(state.weather.lastUpdated).format(context),
+              style: const TextStyle(fontSize: 18.0),
+            ),
+            const SizedBox(width: 10.0),
+            Text(
+              '(${state.weather.country})',
+              style: const TextStyle(fontSize: 18.0),
+            ),
+          ],
+        ),
+        const SizedBox(height: 60.0),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              showTemperature(state.weather.temp),
+              style: const TextStyle(
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(width: 20.0),
+            Column(
+              children: [
+                Text(
+                 'Máx. ${showTemperature(state.weather.tempMax)}',
+                  style: const TextStyle(fontSize: 16.0),
+                ),
+                const SizedBox(height: 10.0),
+                Text(
+                  'Min. ${showTemperature(state.weather.tempMin)}',
+                  style: const TextStyle(fontSize: 16.0),
+                ),
+              ],
+            ),
+          ],
+        ),
+        const SizedBox(height: 40.0),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            const Spacer(),
+            showIcon(state.weather.icon),
+            Expanded(
+              flex: 3,
+              child: formatText(state.weather.description),
+            ),
+            const Spacer(),
+          ],
+        ),
+      ],
+    );
+  }
+
+
+  Widget showIcon(String icon) {
+    return FadeInImage.assetNetwork(
+      placeholder: 'assets/images/loading.gif',
+      image: 'http://$kIconHost/img/wn/$icon@4x.png',
+      width: 96,
+      height: 96,
+    );
+  }
+
+
+  Widget formatText(String description) {
+    final formattedString = description.titleCase;
+    return Text(
+      formattedString,
+      style: const TextStyle(fontSize: 24.0),
+      textAlign: TextAlign.center,
     );
   }
 }
